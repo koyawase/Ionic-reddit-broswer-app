d2f8f3c0e0b86a6bf0cb15cf2a6c62e42d02fbb1
Waseem Koya <waseemkoya@gmail.com>
2018-08-06 22:47:01 +1200

    gathered data needed to store post in firebase. commented inside details.ts

--- Iconic/src/pages/details/details.ts
+++ Iconic/src/pages/details/details.ts
@@ -10,9 +10,20 @@ export class DetailsPage {
     post: any;
     constructor(public navCtrl: NavController, public navParams: NavParams) {
       this.post = navParams.get('post');
+      console.log(this.post);
     }
   
     likePost(){
-      
+      //Post data
+      //string this.post.thumbnail
+      //if this.post.preview != undefined 
+      //boolean this.post.preview.enabled
+      //string this.post.preview.images[0]
+      //string this.post.title
+      //string this.post.author
+      //number this.post.score
+      //number this.post.num_comments
+      //string this.post.permalink
+      //string this.post.selftext
     }
   }
